c36ec5b88d09460ddae4a99ba6538517a7664ebf
Piotr Esden-Tempski <piotr@esden.net>
2020-01-23 18:00:35 -0800

    Decreased LED size and added a status bar.
    * The LED strip was not fitting onto a 1080p display, now it does.
    * Now we have a fast updating text output for debugging purposes. :)

--- twang.py
+++ twang.py
@@ -134,18 +134,20 @@ def screensaver_tick(ledstring, time):
 def main():
 
     # TWANG globals
-    led_size = 16
+    led_size = 13
     led_margin = 1
     led_color = (0, 0, 0)
     led_string_length = 144
     led_string = LEDString(led_string_length, color=led_color, size=led_size, margin=led_margin)
+    led_string_status = 13
 
     # pyGame stuff
     pygame.init()
 
-    screen = pygame.display.set_mode((led_size * led_string_length, led_size))
+    screen = pygame.display.set_mode((led_size * led_string_length, led_size + led_string_status))
     clock = pygame.time.Clock()
     pygame.display.set_caption('pyTWANG')
+    font = pygame.font.Font(None, 20)
 
     # main loop
     running = True
@@ -157,14 +159,28 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    running = False
 
-        screen.fill((255, 255, 255))
+        # Clear screen
+        screen.fill((128, 128, 128))
+
+        # Advance animations
         # led_string.animate()
         screensaver_tick(led_string, pygame.time.get_ticks())
+
+        # Render LEDs onto the screen
         led_string.draw(screen)
 
+        # Draw satusbar information
+        status = font.render("t: {}".format((pygame.time.get_ticks() - starttime) / 1000), True, (0, 0, 0))
+        status_rect = status.get_rect()
+        status_rect.topleft = (10, led_size)
+        screen.blit(status, status_rect)
+
+        # Make things visible
         pygame.display.flip()
-        #print("t: {}".format((pygame.time.get_ticks() - starttime) / 1000))
 
     pygame.quit()
     sys.exit()
